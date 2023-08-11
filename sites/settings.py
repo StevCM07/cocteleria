a657d983fadf77c0bb4a25828078903b861356b6
Steven <steven.cm.1515@gmail.com>
2023-08-11 12:38:26 -0500

    falta el nosotros

--- sites/settings.py
+++ sites/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = 'django-insecure-266c=4vd-zy2jb9u895cuv(yrsxa*=-y874c8fo6#$l3leiv)d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '3.145.18.122',
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
